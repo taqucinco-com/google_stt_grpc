c16baa1b4fa585e9bb0b85f2d3d5b44b2dbbc9a2
taqucinco <someone.said.so@gmail.com>
2026-09-22 15:23:08 +0900

    gRPC sample

--- iOS/google_stt_grpc/google_stt_grpc/ContentView.swift
+++ iOS/google_stt_grpc/google_stt_grpc/ContentView.swift
@@ -6,19 +6,37 @@
 //
 
 import SwiftUI
+import GRPCCore
+import GRPCNIOTransportHTTP2
+import GRPCProtobuf
+import SwiftProtobuf
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+      Button("Click Me") {
+        Task {
+          try await withGRPCClient(
+            transport: .http2NIOPosix(
+              target: .dns(host: "localhost", port: 50051),
+              transportSecurity: .plaintext
+            )
+          ) { client in
+            let greeter = Greeter_Greeter.Client(wrapping: client)
+            let reply = try await greeter.sayHello(.with { $0.name = "taro" })
+            print(reply.message)  // ここがdebug consoleに出る
+          }
         }
-        .padding()
+      }
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
